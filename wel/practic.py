36d72fbdc9da018872fe30c9bee0cb0505902412
Farida <muslimaesanalieva@gmail.com>
2026-03-23 15:34:56 +0200

    first commit

---
+++ wel/practic.py
@@ -0,0 +1,9 @@
+def ball(name,prd,ball):
+    j ={'name':name,'prd':prd,'ball':ball,}
+    return j
+print(ball('Sara','cricket',100))
+print(ball('Dhoni','cricket',50))
+print(ball('Virat','cricket',70))
+
+    
+
