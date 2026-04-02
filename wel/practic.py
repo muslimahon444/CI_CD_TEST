e127ff91bef6365ef34400bdaac67bab62694fd9
Farida <muslimaesanalieva@gmail.com>
2026-04-02 07:13:46 +0300

    лабараторная 11 намеренную ошибку

--- wel/practic.py
+++ wel/practic.py
@@ -5,5 +5,16 @@ print(ball('Sara','cricket',100))
 print(ball('Dhoni','cricket',50))
 print(ball('Virat','cricket',70))
 
+def code(x):
+    for o in x:
+        if o == int(x):
+            res = x*2
+            return res
+        else:
+            print(x)
+
+print(code(6))
+t =2000,
+
     
 
